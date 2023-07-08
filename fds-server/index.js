88dfddcc2ba7cc3bef8adc9d6a01cdb75e54a060
Christeena <christinaborah50@gmail.com>
2023-07-08 14:38:30 +0530

    added changes to index file

--- fds-server/index.js
+++ fds-server/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+require('./config/db')
+
+const MerchantRouter = require('./routes/merchantRoute')
+const HomeRouter = require('./routes/homeRoute')
+
+const port = process.env.PORT;
+const apiVersion = process.env.API_VERSION;
+const apiBasePath = `/api/${apiVersion}`;
+
 const app = express();
-const port = 5000;
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+    res.send('Server is running');
+})
+
+// Home router
+app.use(`${apiBasePath}`, HomeRouter);
+
+// Register the Merchant router
+app.use(`${apiBasePath}`, MerchantRouter);
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
